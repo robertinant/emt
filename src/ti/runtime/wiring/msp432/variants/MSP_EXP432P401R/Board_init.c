fab873f3db1c4b6e37054c696fd568dd7fc61605
Alan DeMars <ademars@ti.com>
2017-06-15 15:28:14 -0700

    ENERGIAMT-11 NVS driver integration into Energia

--- src/ti/runtime/wiring/msp432/variants/MSP_EXP432P401R/Board_init.c
+++ src/ti/runtime/wiring/msp432/variants/MSP_EXP432P401R/Board_init.c
@@ -719,6 +719,38 @@ const I2C_Config I2C_config[] = {
 const uint_least8_t I2C_count = Board_I2CCOUNT;
 
 /*
+ *  =============================== NVS ===============================
+ *  Non-Volatile Storage configuration.
+ */
+#include <ti/drivers/NVS.h>
+#include <ti/drivers/nvs/NVSMSP432.h>
+
+NVSMSP432_Object nvsMSP432Objects[1];
+
+extern uint8_t __NVS_BASE__;
+extern uint8_t __NVS_SIZE__;
+
+NVSMSP432_HWAttrs nvsMSP432HWAttrs[1] = {
+    /*
+     * region 0 is 1 flash sector in length.
+     */
+    {
+        .regionBase = (void *)&__NVS_BASE__,
+        .regionSize = (size_t)(&__NVS_SIZE__)
+    },
+};
+
+const NVS_Config NVS_config[] = {
+    {
+        &NVSMSP432_fxnTable,
+        &nvsMSP432Objects[0],
+        &nvsMSP432HWAttrs[0]
+    },
+};
+
+int NVS_count = 1;
+
+/*
  *  =============================== Power ===============================
  */
 
